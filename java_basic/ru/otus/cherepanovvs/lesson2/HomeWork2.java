b34597041c4198084667e4ee7685cf406763b811
Che1989 <che3108@gmail.com>
2023-11-09 23:11:30 +0800

    создал склет проекта hw2

---
+++ java_basic/ru/otus/cherepanovvs/lesson2/HomeWork2.java
@@ -0,0 +1,12 @@
+package ru.otus.cherepanovvs.lesson2;
+
+public class HomeWork2 {
+    public static void main() {
+        simpleGussNumber();
+    }
+
+    public static void simpleGussNumber() {
+        final String greetings = "Здравствуй, путник!\nТебе необходимо угадать число, которое я загадал.";
+        System.out.println(greetings);
+    }
+}
