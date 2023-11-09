5fa8cd210450c70a60f02c2239ed86549ae55b69
Che1989 <che3108@gmail.com>
2023-11-10 00:17:58 +0800

    Сделал простую игру в угадай число

--- java_basic/ru/otus/cherepanovvs/lesson2/HomeWork2.java
+++ java_basic/ru/otus/cherepanovvs/lesson2/HomeWork2.java
@@ -1,5 +1,7 @@
 package ru.otus.cherepanovvs.lesson2;
 
+import java.util.Scanner;
+
 public class HomeWork2 {
     public static void main(String[] args) {
         simpleGussNumber();
@@ -7,6 +9,23 @@ public class HomeWork2 {
 
     public static void simpleGussNumber() {
         final String greetings = "Здравствуй, путник!\nТебе необходимо угадать число, которое я загадал.";
+        final int scaleCoefficient = 10;
+
+        int hiddenNumber;
+        int answerNumber;
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(greetings);
+        System.out.println("Загаданное число в диапазоне от 0 до " + (scaleCoefficient -1) + ".");
+
+        hiddenNumber = (int)(Math.random() * scaleCoefficient);
+        answerNumber = scanner.nextInt();
+
+        if (answerNumber == hiddenNumber) {
+            System.out.println("Да, все верно.");
+        } else {
+            System.out.println("Нет, не угадал.");
+        }
     }
 }
