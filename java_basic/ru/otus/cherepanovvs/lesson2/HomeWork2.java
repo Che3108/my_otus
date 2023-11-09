c7d85fae1bb6674881a11ad831a1df0ebc1385d3
Che1989 <che3108@gmail.com>
2023-11-10 02:50:32 +0800

    усложнил игру

--- java_basic/ru/otus/cherepanovvs/lesson2/HomeWork2.java
+++ java_basic/ru/otus/cherepanovvs/lesson2/HomeWork2.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        simpleGussNumber();
+        difficultGussNumberGame();
     }
-
-    public static void simpleGussNumber() {
+    
+    public static void simpleGussNumberGame() {
         final String greetings = "Здравствуй, путник!\nТебе необходимо угадать число, которое я загадал.";
         final int scaleCoefficient = 10;
 
@@ -28,5 +28,64 @@ public class HomeWork2 {
         } else {
             System.out.println("Нет, не угадал.");
         }
+    }
+
+    public static void difficultGussNumberGame() {
+        int scaleCoefficient;
+        int tryingCount;
+        int hiddenNumber;
+        int answerNumber;
+        int difficultyGame;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Здравствуй, путник!\nТебе необходимо угадать число, которое я загадал.");
+        System.out.println("Но в начале выбери сложность:\n1 - легко\n2 - средне\n3 - сложно");
+        difficultyGame = scanner.nextInt();
+        while (true) {
+            if (difficultyGame == 1 || difficultyGame == 2 || difficultyGame == 3) {
+                break;
+            } else {
+                System.out.println("Выбери, пожалуйста, сложность...");
+                difficultyGame = scanner.nextInt();
+            }
+        }
+        
+        switch (difficultyGame) {
+            case 1:
+                scaleCoefficient = 10;
+                tryingCount = 4;
+                break;
+            case 2:
+                scaleCoefficient = 15;
+                tryingCount = 3;
+                break;
+            case 3:
+                scaleCoefficient = 20;
+                tryingCount = 2;
+                break;
+            default:
+                scaleCoefficient = 100;
+                tryingCount = 1;
+                break;
+        }
+
+        hiddenNumber = (int)(Math.random() * scaleCoefficient);
+        System.out.println("Загаданное число в диапазоне от 0 до " + (scaleCoefficient -1));
+        for (; tryingCount != 0; tryingCount--) {
+            System.out.println("Осталость попыток: " + tryingCount);
+            answerNumber = scanner.nextInt();
+            if (answerNumber == hiddenNumber) {
+                System.out.println("Да, все верно!");
+                break;
+            }
+            if (answerNumber > hiddenNumber) {
+                System.out.println("Загаданное число меньше, чем " + answerNumber);
+            }
+            if (answerNumber < hiddenNumber) {
+                System.out.println("Загаданное число больше, чем " + answerNumber);
+            }
+        }
+
+        scanner.close();
     }
 }
