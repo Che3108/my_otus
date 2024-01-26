0395a986ec319da0c25848ee936c36d56acb9c04
Che1989 <che3108@gmail.com>
2024-01-27 00:35:17 +0800

    устранил ошибку старта потоков

--- java_basic/ru/otus/cherepanovvs/lesson21/ManyThreads.java
+++ java_basic/ru/otus/cherepanovvs/lesson21/ManyThreads.java
@@ -2,24 +2,36 @@ package ru.otus.cherepanovvs.lesson21;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManyThreads {
     public static void main(String[] args) {
         double[] xSpace = new double[100_000_000];
+
+        List<Thread> threads = new ArrayList<>();
 
         Thread thread1 = new Thread(() -> calculateFunc(xSpace, 0, 25_000_000));
         Thread thread2 = new Thread(() -> calculateFunc(xSpace, 25_000_000, 50_000_000));
         Thread thread3 = new Thread(() -> calculateFunc(xSpace, 50_000_000, 75_000_000));
         Thread thread4 = new Thread(() -> calculateFunc(xSpace, 75_000_000, 100_000_000));
 
+        threads.add(thread1);
+        threads.add(thread2);
+        threads.add(thread3);
+        threads.add(thread4);
+
         Instant start = Instant.now();
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (Thread t: threads) {
+            t.start();            
+        }
+
+        for (Thread t: threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }         
         }
         Instant finish = Instant.now();
         long elapsed = Duration.between(start, finish).toMillis();
